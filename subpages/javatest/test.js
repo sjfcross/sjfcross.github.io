aa856d801257f5c9868d66501ecacecda305aa52
sjfcross <81625110+sjfcross@users.noreply.github.com>
2021-08-13 21:10:10 +0100

    h
    h

---
+++ subpages/javatest/test.js
@@ -0,0 +1,10 @@
+var links = document.getElementsByTagName("a");
+
+for (i = 1; i<= links.length; i++) {
+
+    console.log("this is link number" + i);
+
+}
+
+document.write("all links now looped");
+
