1dc9849f57bd6bd76e368885a325fd28bb8fde49
sjfcross <81625110+sjfcross@users.noreply.github.com>
2021-08-16 11:59:37 +0100

    Update test2.js

--- subpages/javatest/test2.js
+++ subpages/javatest/test2.js
@@ -1,19 +1,31 @@
-
-
 for (i = 1; i < 10; i++) {
-
-if (i ===5 || i === 3) {
-
-    continue;
-
-}
-
-    console.log("i");
-
-if(i === 7) {
-
-    break;
+  
+  
+    if(i ===5 || i ===3) {
+      
+      continue;
+    }
+ console.log(i);
+   
+   if(i === 7) {
+         
+   break;
+    
+     }
+ 
    }
-}
+ 
+ console.log("I have broken out of the loop");
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
 
