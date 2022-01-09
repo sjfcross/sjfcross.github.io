7146b1be904ca69aeadc96641c73c86320b4c9a7
sjfcross <81625110+sjfcross@users.noreply.github.com>
2022-01-09 01:35:17 +0000

    o
    o

---
+++ subpages/coltsteele/Running_JS_Script/script.js
@@ -0,0 +1,65 @@
+console.log("Hello from our first JS - File, Mofo !");
+let total = 1 + 4;
+console.log("Goodby !! !");
+
+console.log("Next Conditionals");
+
+console.log("Before Conditional");
+if (1 + 1 === 2) {
+  console.log("Math still works!");
+}
+console.log("After Conditionals");
+
+let random = Math.random();
+if (random < 0.5) {
+  console.log("Your number is less then 0.5!!!");
+} else {
+    console.log("Your number is greater (or equal) then 0.5!!!!")
+}
+console.log(random);
+
+// if (random >= 0.5) {
+//   console.log("Your number is BIGGER then 0.5!!!");
+//   console.log(random);
+// }
+
+// const dayOfWeek = prompt("Enter a day").toLowerCase();
+
+// if (dayOfWeek === "monday") {
+//   console.log("Uhggg I hate Mondays");
+// } else if (dayOfWeek === "saturday") {
+//   console.log("Yeeeah I love Saturdays");
+// } else if (dayOfWeek === "friday") {
+//   console.log("Cool, Friday is alright!");
+// } else {
+//     console.log("Meh")
+// }
+
+  //  else if (dayOfWeek === "Tuesday") {
+//   console.log("Meh");
+// } else if (dayOfWeek === "Wednesday") {
+//   console.log("Meh");
+// } else if (dayOfWeek === "Thursday") {
+//   console.log("Meh");
+// } else if (dayOfWeek === "Sunday") {
+//   console.log("Meh");
+// }
+
+
+
+
+// 0-5 - FREE
+// 5-10 CHILD $10
+// 10 - 65 ADULT $20
+// 65+ SENIOR $10
+
+// const age = 65;
+// if (age < 5) {
+//   console.log("You are a baby. You get in for free !");
+// } else if (age < 10) {
+//   console.log("You are between 5y and 10y old. Pay $10 !");
+// } else if (age < 65) {
+//   console.log("You are an adult. Pay up all the $20!");
+// } else if (age >= 65) {
+//   console.log("You are a senior. $10 is enough for u!");
+// }
