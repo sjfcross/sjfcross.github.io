16b51bd6184d82831cfb2d3fdab1af27a6a667c4
sjfcross <81625110+sjfcross@users.noreply.github.com>
2022-01-16 21:35:45 +0000

    s
    s

--- subpages/coltsteele/loops/TodoListProject/todos.js
+++ subpages/coltsteele/loops/TodoListProject/todos.js
@@ -1,25 +1,58 @@
-let input = prompt('what would you like to do?');
-const todos = ['Collect Chicken Eggs', 'Clean Litter Box'];
-while (input !== 'quit' && input !== 'q') {
-    if (input === 'list') {
-        console.log('*****************')
-        for (let i = 0; i < todos.length; i++) {
-            console.log(`${i}: ${todos[i]}`);
-        }
-        console.log('*****************')
-    } else if (input === 'new') {
-        const newTodo = prompt('Ok, what is the new todo?');
-        todos.push(newTodo);
-        console.log(`${newTodo} added to the list!`)
-    } else if (input === 'delete') {
-        const index = parseInt(prompt('Ok, enter an index to delete:'));
-        if (!Number.isNaN(index)) {
-            const deleted = todos.splice(index, 1);
-            console.log(`Ok, deleted ${deleted[0]}`);
-        } else {
-            console.log('Unknown index')
-        }
+// let input = prompt('what would you like to do?');
+// const todos = ['Collect Chicken Eggs', 'Clean Litter Box'];
+// while (input !== 'quit' && input !== 'q') {
+//     if (input === 'list') {
+//         console.log('*****************')
+//         for (let i = 0; i < todos.length; i++) {
+//             console.log(`${i}: ${todos[i]}`);
+//         }
+//         console.log('*****************')
+//     } else if (input === 'new') {
+//         const newTodo = prompt('Ok, what is the new todo?');
+//         todos.push(newTodo);
+//         console.log(`${newTodo} added to the list!`)
+//     } else if (input === 'delete') {
+//         const index = parseInt(prompt('Ok, enter an index to delete:'));
+//         if (!Number.isNaN(index)) {
+//             const deleted = todos.splice(index, 1);
+//             console.log(`Ok, deleted ${deleted[0]}`);
+//         } else {
+//             console.log('Unknown index')
+//         }
+//     }
+//     input = prompt('what would you like to do?')
+// }
+// console.log('OK QUIT THE APP!')
+
+let input = prompt("what would you like to do?");
+
+const todos = ["Collect Chicken Eggs", "Clean Cat Litter Box"];
+
+while (input !== "quit" && input !== "q") {
+  if (input === "list") {
+    console.log("****************");
+
+    for (let i = 0; i < todos.length; i++) {
+      console.log(`${i} : ${todos[i]}`);
     }
-    input = prompt('what would you like to do?')
+    console.log("****************");
+  } else if (input === "new") {
+    const newTodo = prompt("Ok, what is the new todo ?");
+    todos.push(newTodo);
+    console.log(`${newTodo} added to List !`);
+  } else if (input === "delete") {
+    const index = parseInt(
+      prompt("Ok, which Index would you like to delete ?")
+    );
+    if (!Number.isNaN(index)) {
+      const deleted = todos.splice(index, 1);
+      console.log(`Ok, deleted ${deleted[0]}`);
+    } else {
+      console.log("Unknown Index");
+    }
+  }
+
+  input = prompt("What would you like to do?");
 }
-console.log('OK QUIT THE APP!')
+
+console.log("Ok, quit the app !");
