928132c11de41610d3ab0db235de43c4e836ace5
sjfcross <81625110+sjfcross@users.noreply.github.com>
2022-08-14 19:20:29 +0100

    meth
    meth

--- subpages/coltsteele/definingmethods/app.js
+++ subpages/coltsteele/definingmethods/app.js
@@ -11,3 +11,15 @@ const myMath = {
         return num ** 3;
     }
 }
+
+// not working for me : //
+
+// const myMath = {
+//     PI: 3.14159,
+//     square:(num) {
+//         return num * num;
+//     },
+//     cube:(num) {
+//         return num ** 3;
+//     }
+// }
