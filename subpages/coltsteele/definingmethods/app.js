f89d7f83533cf63cabff14c5f249e58487b017d2
sjfcross <81625110+sjfcross@users.noreply.github.com>
2022-08-14 18:18:32 +0100

    new
    as

---
+++ subpages/coltsteele/definingmethods/app.js
@@ -0,0 +1,13 @@
+// ======================
+// Defining Methods
+// ======================
+
+const myMath = {
+    PI: 3.14159,
+    square: function (num) {
+        return num * num;
+    },
+    cube: function (num) {
+        return num ** 3;
+    }
+}
